1fe52dbce010ed2e0052ba2fd22617bcb7284631
Logeswaran A <lokeshwaran.a82@gmail.com>
2022-03-19 09:20:01 +0400

    updated

---
+++ NetworkMgmt/index.js
@@ -0,0 +1,47 @@
+const { getCCP,getWallets,buildWallet } = require("../IdentityManagement/IdentityManagement.js");
+//Gateway 
+const { Wallets, Gateway } = require('fabric-network');
+const path = require("path");
+
+exports.invokeTransaction = async (cp, channelName, chaincodeName, functionName, request, schema) => {
+    
+    let org = request.user.org;
+
+    console.log("tetingigigg",request.user.org,request.user)
+    let num = Number(org.match(/\d/g).join(""));
+
+    const ccp = getCCP(num);
+
+    const walletPath = await getWallets(num);
+
+    console.log("wallet path is",walletPath);
+
+    const wallet = await buildWallet(Wallets, walletPath);
+
+    const gateway = new Gateway();
+
+    await gateway.connect(ccp, {
+        wallet,
+        identity: request.user.userName,
+        discovery: { enabled: true, asLocalhost: false } // using asLocalhost as this gateway is using a fabric network deployed locally
+    });
+
+    // Build a network instance based on the channel where the smart contract is deployed
+    const network = await gateway.getNetwork(channelName);
+
+    // Get the contract from the network.
+    const contract = network.getContract(chaincodeName);
+
+    var userinputs = "";
+
+    if (request.originalUrl.indexOf('/delete') >= 0) {
+        userinputs = request.body.id;
+    }
+
+    console.log("userinputs is ",userinputs)
+    const transaction = contract.createTransaction(functionName);
+    const result = await transaction.submit(userinputs);
+    var obj = {"Success":true,"Txn ID":transaction.getTransactionId(),"statusCode":200};
+
+    return obj;
+}
